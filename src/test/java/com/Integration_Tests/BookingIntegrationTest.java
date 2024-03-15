72c7ddbb40bfdbf24185f79dc80429c82e85cf12
Martin Ceron Oviedo <95503977+MartinCeron@users.noreply.github.com>
2024-03-15 13:29:46 -0400

    Update BookingIntegrationTest.java

--- src/test/java/com/Integration_Tests/BookingIntegrationTest.java
+++ src/test/java/com/Integration_Tests/BookingIntegrationTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BookingIntegrationTest {
+
+    @Test
+    void testBookDirectFlight() {
+        assertTrue(BookingManager.bookDirectFlight("ABC123", "2024-03-15", "08:00", "12:00"));
+    }
+
+    @Test
+    void testBookRoundTrip() {
+        assertTrue(BookingManager.bookRoundTrip("XYZ456", "2024-03-15", "12:00", "2024-03-20", "15:00"));
+    }
+
+    // More integration tests can be added to cover other scenarios
 }
+
